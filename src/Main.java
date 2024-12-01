951626a1939f4460c418fed75d3413f316a86019
Hindustanin <hindustanin@hotmail.com>
2024-12-01 20:51:02 +0700

    Старт

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
 
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + ' ' + firstName + ' ' + lastName;
+        System.out.println(fullName);
+
+        // Задача 2
+        System.out.println();
+        System.out.println("Задача 2");
+
+        String small = fullName;
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + small.toUpperCase());
+
+        // Задача 3
+        System.out.println();
+        System.out.println("Задача 3");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё" , "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName);
     }
 }
