337f6ba9008a0a68050b2e6d61bb0e15383e6bcd
Hindustanin <hindustanin@hotmail.com>
2024-12-01 22:06:40 +0700

    Исправил задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         // Задача 1
         System.out.println("Задача 1");
 
         String firstName = "Ivan";
-        String middleName = "Ivanov";
-        String lastName = "Ivanovich";
-        String fullName = middleName + ' ' + firstName + ' ' + lastName;
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
         System.out.println(fullName);
 
         // Задача 2
